94f6b74365dc26e27c19dd072589d74a79c5258e
Vinay0047-VK <vinaydev0047@gmail.com>
2026-05-12 20:32:58 +0530

    Basic Web structure added

---
+++ web-proj/src/Cars.jsx
@@ -0,0 +1,9 @@
+const Card =()=>{
+    return(
+        <div className="card">
+            <h2>Car Name</h2>
+            <p>Car Description</p>
+        </div>
+    )
+}
+export default Card;
---
+++ web-proj/src/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <div>
+        <footer className="main-footer">
+            <div className="container">
+            <p>&copy; 2024 My Website. All rights reserved.</p>
+            </div>
+        </footer>
+    </div>
+  );
+}
+export default Footer;
